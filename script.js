94937e7a0f266fda463a943de49831a9cbc36d85
Cesar <tuemailexample.com>
2026-03-11 19:58:17 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// This selects the button by its ID and adds a click event
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Hello! The code is working.');
+});
