d5518584f6835abaf4a5c3a63caf2b41c8ae5af4
Cesar <tuemailexample.com>
2026-03-13 09:15:21 -0500

    Overview_buil

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// This selects the button by its ID and adds a click event
-document.getElementById('alertButton').addEventListener('click', function() {
-    alert('Hello! The code is working.');
-});
+// Seleccionamos los elementos de la pantalla
+const modal = document.getElementById("eclModal");
+const btn = document.getElementById("openModalBtn");
+const span = document.getElementsByClassName("close-btn")[0];
+
+// Cuando el usuario hace clic en el botón, se abre el modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Cuando el usuario hace clic en la "X", se cierra el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario hace clic fuera de la ventana blanca, también se cierra
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
